ea7a9c0f2c60db317b6ff32dd6cddce9c439a7c0
Antoine Amiguet <amiguet.antoine@gmail.com>
2021-02-08 21:58:40 +0100

    1.5.5 Exercise: Equality, Liberty, and Felinity

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -4,6 +4,10 @@ package sandbox
 //import cats.syntax.semigroup._
 
 import cats.syntax.show._
+import cats.syntax.eq._
+import cats.syntax.option._
+
+import cats.instances.option._
 
 import sandbox.typeclass.Cat
 import sandbox.typeclass.PrintableSyntax._
@@ -12,4 +16,13 @@ object Main extends App {
   val cat = Cat("Garfield", 14, "orange")
   cat.print
   println(cat.show)
+
+  val cat1 = Cat("Garfield", 38,"orange and black")
+  val cat2 = Cat("Heathcliff", 33,"orange and black")
+
+  println(cat1 === cat2)
+  println(cat1 === cat1)
+  println(none[Cat] === none[Cat])
+  println(cat1.some =!= cat2.some)
+  println(cat1.some === cat1.some)
 }
--- src/main/scala/sandbox/typeclass/Cat.scala
+++ src/main/scala/sandbox/typeclass/Cat.scala
@@ -3,9 +3,11 @@ package sandbox.typeclass
 import PrintableInstances._
 
 import cats.Show
+import cats.Eq
 import cats.instances.string._
 import cats.instances.int._
 import cats.syntax.show._
+import cats.syntax.eq._
 
 final case class Cat(name: String, age: Int, color: String)
 
@@ -18,4 +20,7 @@ object Cat {
 
   implicit val catShow: Show[Cat] =
     Show.show(cat => s"${cat.name.show} is a ${cat.age.show} year-old ${cat.color.show} cat.")
+
+  implicit val catEq: Eq[Cat] =
+    Eq.instance((x: Cat, y: Cat) => x.name === y.name && x.age === y.age && x.color === y.color)
 }
